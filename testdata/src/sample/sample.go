d835c8439c65c779df5b9d6753a7f9c2fe9511e9
moricho <ikeda.morito@gmail.com>
2020-09-03 10:26:25 +0900

    [WIP] get t.Parallel and analize ssa

--- testdata/src/sample/sample.go
+++ testdata/src/sample/sample.go
@@ -18,7 +18,7 @@ func setup(name string) func() {
 
 func Test_Func1(t *testing.T) { // NG: cleanup
 	teardown := setup("Test_Func1")
-	defer teardown()
+	defer teardown() // want "Test_Func1 should use t.Cleanup() instead of defer"
 
 	t.Parallel()
 
@@ -33,67 +33,67 @@ func Test_Func1(t *testing.T) { // NG: cleanup
 	})
 }
 
-func Test_Func2(t *testing.T) { // NG: parallel
-	teardown := setup("Test_Func2")
-	t.Cleanup(teardown)
+// func Test_Func2(t *testing.T) { // NG: parallel
+// 	teardown := setup("Test_Func2")
+// 	t.Cleanup(teardown)
 
-	t.Run("Func2_Sub1", func(t *testing.T) {
-		call("Func2_Sub1")
-		t.Parallel()
-	})
+// 	t.Run("Func2_Sub1", func(t *testing.T) {
+// 		call("Func2_Sub1")
+// 		t.Parallel()
+// 	})
 
-	t.Run("Func2_Sub2", func(t *testing.T) {
-		call("Func2_Sub2")
-		t.Parallel()
-	})
-}
+// 	t.Run("Func2_Sub2", func(t *testing.T) {
+// 		call("Func2_Sub2")
+// 		t.Parallel()
+// 	})
+// }
 
-func Test_Func6(t *testing.T) { // NG: parallel
-	teardown := setup("Test_Func6")
-	t.Cleanup(teardown)
+// func Test_Func6(t *testing.T) { // NG: parallel
+// 	teardown := setup("Test_Func6")
+// 	t.Cleanup(teardown)
 
-	t.Parallel()
+// 	t.Parallel()
 
-	t.Run("Func6_Sub1", func(t *testing.T) {
-		call("Func6_Sub1")
-	})
+// 	t.Run("Func6_Sub1", func(t *testing.T) {
+// 		call("Func6_Sub1")
+// 	})
 
-	t.Run("Func6_Sub2", func(t *testing.T) {
-		call("Func6_Sub2")
-	})
-}
+// 	t.Run("Func6_Sub2", func(t *testing.T) {
+// 		call("Func6_Sub2")
+// 	})
+// }
 
-func Test_Func3(t *testing.T) { // OK
-	teardown := setup("Test_Func3")
-	t.Cleanup(teardown)
-	t.Parallel()
+// func Test_Func3(t *testing.T) { // OK
+// 	teardown := setup("Test_Func3")
+// 	t.Cleanup(teardown)
+// 	t.Parallel()
 
-	t.Run("Func3_Sub1", func(t *testing.T) {
-		call("Func3_Sub1")
-		t.Parallel()
-	})
+// 	t.Run("Func3_Sub1", func(t *testing.T) {
+// 		call("Func3_Sub1")
+// 		t.Parallel()
+// 	})
 
-	t.Run("Func3_Sub2", func(t *testing.T) {
-		call("Func3_Sub2")
-		t.Parallel()
-	})
-}
+// 	t.Run("Func3_Sub2", func(t *testing.T) {
+// 		call("Func3_Sub2")
+// 		t.Parallel()
+// 	})
+// }
 
-func Test_Func4(t *testing.T) { // OK
-	teardown := setup("Test_Func4")
-	defer teardown()
-	t.Parallel()
+// func Test_Func4(t *testing.T) { // OK
+// 	teardown := setup("Test_Func4")
+// 	defer teardown()
+// 	t.Parallel()
 
-	t.Run("Func4_Sub1", func(t *testing.T) {
-		call("Func4_Sub1")
-	})
+// 	t.Run("Func4_Sub1", func(t *testing.T) {
+// 		call("Func4_Sub1")
+// 	})
 
-	t.Run("Func4_Sub2", func(t *testing.T) {
-		call("Func4_Sub2")
-	})
-}
+// 	t.Run("Func4_Sub2", func(t *testing.T) {
+// 		call("Func4_Sub2")
+// 	})
+// }
 
-func Test_Func5(t *testing.T) { // OK
-	teardown := setup("Test_Func5")
-	defer teardown()
-}
+// func Test_Func5(t *testing.T) { // OK
+// 	teardown := setup("Test_Func5")
+// 	defer teardown()
+// }
